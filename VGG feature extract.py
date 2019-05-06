18d420730f8c8b3c2a3f7447e38bb103fa7491a3
Willem <willem_diep@hotmail.com>
2019-05-06 12:36:54 +0200

    VGG met goede output

---
+++ VGG feature extract.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.vgg16 import decode_predictions
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Reshape
+import numpy as np
+# load the model
+model = VGG16()
+
+# make new model by deleting last layer
+new_input = model.input
+hidden_layer = model.layers[-6].output
+new_output = Reshape((196, 512), input_shape = (14, 14, 512))(hidden_layer)
+
+image_features_extract_model = Model(inputs = new_input, outputs = new_output)
+image_features_extract_model.summary()
+
+
+# test for coco examples
+test = 'data/mug.jpg'
+
+
+# load an image from file
+image = load_img(test, target_size=(224, 224))
+# convert the image pixels to a numpy array
+image = img_to_array(image)
+# reshape data for the model
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+# prepare the image for the VGG model
+image = preprocess_input(image)
+# predict the probability across all output classes
+yhat = image_features_extract_model.predict(image)
