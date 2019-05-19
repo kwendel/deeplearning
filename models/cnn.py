ac21d96354231910f37a3e14d27420957e96747e
Kasper Wendel <kasperwendel@gmail.com>
2019-05-19 15:36:06 +0200

    use the pretrained VGG for data prediction

--- models/cnn.py
+++ models/cnn.py
@@ -1,11 +1,12 @@
-# from tensorflow.python.keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.python.keras.layers import Reshape, Dense
-from tensorflow.python.keras import Model
+from os import path
 
-from tensorflow.python.keras.applications.vgg16 import VGG16
-# from keras.applications.vgg16 import VGG16
+from tensorflow.python.keras.applications import VGG16
+from tensorflow.python.keras.layers import Reshape, Dense
+from tensorflow.python.keras.models import Model
 
 from utils.hparams import Hparams
+
+PRETRAINED_VGG16 = 'pretrained/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
 
 
 class CNN:
@@ -41,20 +42,24 @@ class CNN:
 class DataPredictor:
     def __init__(self):
         # Construct the pretrained VGG16
-        model = VGG16()
-        print("## VGG unchanged")
-        model.summary()
+        dir = path.dirname(__file__)
+        weights_path = path.join(dir, PRETRAINED_VGG16)
+        model = VGG16(include_top=True, weights=weights_path)
+
+        # Output the original network structure for debuggin
+        # print("## VGG unchanged")
+        # model.summary()
 
         # Change the model to output his intermediate predictions
         new_input = model.input
-        semantic_output = model.layers[-1].output
-        hidden_layer = model.layers[-6].output
+        new_output = model.layers[-6].output
+        reshaper = Reshape((196, 512), input_shape=(14, 14, 512))(new_output)
 
+        # Check if architecture of the VGG with intermediate predictions
         print("## VGG with intermediate predictions")
-        self.vgg = Model(inputs=new_input, outputs=[hidden_layer, semantic_output])
-        self.vgg.summary()
+        model = Model(inputs=new_input, outputs=reshaper)
+
+        self.vgg = model
 
     def predict(self, x):
         return self.vgg.predict(x)
-
-
