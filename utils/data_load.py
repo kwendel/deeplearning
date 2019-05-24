42ba1f6f37dcaf3dec48eac73b4c6714c2b77009
niekvanderlaan <niekvanderlaan@outlook.com>
2019-05-24 13:30:50 +0200

    added encoder ff to the network

--- utils/data_load.py
+++ utils/data_load.py
@@ -26,25 +26,24 @@ def load_vocab(vocab_fpath):
     idx2token = {idx: token for idx, token in enumerate(vocab)}
     return token2idx, idx2token
 
-def load_data(fpath1, fpath2, maxlen1, maxlen2):
+def load_data(fpath, maxlen):
     '''Loads source and target data and filters out too lengthy samples.
-    fpath1: source file path. string.
-    fpath2: target file path. string.
-    maxlen1: source sent maximum length. scalar.
-    maxlen2: target sent maximum length. scalar.
+    fpath: source file path. string.
+    maxlen: target sent maximum length. scalar.
 
     Returns
-    sents1: list of source sents
-    sents2: list of target sents
+    sents: list of target sents
     '''
-    sents1, sents2 = [], []
-    with open(fpath1, 'r', encoding='utf-8') as f1, open(fpath2, 'r', encoding='utf-8') as f2:
-        for sent1, sent2 in zip(f1, f2):
-            if len(sent1.split()) + 1 > maxlen1: continue # 1: </s>
-            if len(sent2.split()) + 1 > maxlen2: continue  # 1: </s>
-            sents1.append(sent1.strip())
-            sents2.append(sent2.strip())
-    return sents1, sents2
+    tuples = []
+    with open(fpath, 'r', encoding='utf-8') as f1:
+        for line in f1:
+            #TODO: split line at separation symbol, combine into tuple (image, sent), append to tuples
+            sent = ""
+            image = []
+            if len(sent.split()) + 1 > maxlen: 
+                continue # 1: </s>
+            tuples.append((image, sent))
+    return tuples
 
 
 def encode(inp, type, dict):
@@ -89,10 +88,34 @@ def generator_fn(sents1, sents2, vocab_fpath):
         x_seqlen, y_seqlen = len(x), len(y)
         yield (x, x_seqlen, sent1), (decoder_input, y, y_seqlen, sent2)
 
-def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
+def generator_fn_new(tuples, vocab_fpath):
+    '''Generates training / evaluation data
+    tuples: list of (image feature vector, target sent) tuples
+    vocab_fpath: string. vocabulary file path.
+
+    yields
+    xs: tuple of
+        x: list of source image feature vectors
+    labels: tuple of
+        decoder_input: decoder_input: list of encoded decoder inputs
+        y: list of target token ids in a sent
+        y_seqlen: int. sequence length of y
+        sent: str. target sentence
+    '''
+    token2idx, _ = load_vocab(vocab_fpath)
+    for (image, sent) in tuples:
+        #todo: encode image/not needed?
+        x = image
+        y = encode(sent, "y", token2idx)
+        decoder_input, y = y[:-1], y[1:]
+
+        y_seqlen = len(x), len(y)
+        yield (x), (decoder_input, y, y_seqlen, sent)
+
+def input_fn(images, sents, batch_size, shuffle=False):
     '''Batchify data
-    sents1: list of source sents
-    sents2: list of target sents
+    images: list of source image feature vectors
+    sents: list of target sents
     vocab_fpath: string. vocabulary file path.
     batch_size: scalar
     shuffle: boolean
@@ -108,18 +131,20 @@ def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
         y_seqlen: int32 tensor. (N, )
         sents2: str tensor. (N,)
     '''
-    shapes = (([None], (), ()),
-              ([None], [None], (), ()))
-    types = ((tf.int32, tf.int32, tf.string),
-             (tf.int32, tf.int32, tf.int32, tf.string))
-    paddings = ((0, 0, ''),
-                (0, 0, 0, ''))
+    # shapes = (([None], (), ()),
+    #           ([None], [None], (), ()))
+    # types = ((tf.int32, tf.int32, tf.string),
+    #          (tf.int32, tf.int32, tf.int32, tf.string))
+    # paddings = ((0, 0, ''),
+    #             (0, 0, 0, ''))
+
+    #TODO: Replace above comments with own shapes/types/paddings
 
     dataset = tf.data.Dataset.from_generator(
         generator_fn,
         output_shapes=shapes,
         output_types=types,
-        args=(sents1, sents2, vocab_fpath))  # <- arguments for generator_fn. converted to np string arrays
+        args=(images, sents, vocab_fpath))  # <- arguments for generator_fn. converted to np string arrays
 
     if shuffle: # for training
         dataset = dataset.shuffle(128*batch_size)
@@ -129,7 +154,7 @@ def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
 
     return dataset
 
-def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, batch_size, shuffle=False):
+def get_batch(fpath, maxlen, vocab_fpath, batch_size, shuffle=False):
     '''Gets training / evaluation mini-batches
     fpath1: source file path. string.
     fpath2: target file path. string.
@@ -144,7 +169,7 @@ def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, batch_size, shuffle
     num_batches: number of mini-batches
     num_samples
     '''
-    sents1, sents2 = load_data(fpath1, fpath2, maxlen1, maxlen2)
-    batches = input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=shuffle)
-    num_batches = calc_num_batches(len(sents1), batch_size)
+    image_caption_tuples = load_data(fpath, maxlen)
+    batches = input_fn(image_caption_tuples, batch_size, shuffle=shuffle)
+    num_batches = calc_num_batches(len(sents), batch_size)
     return batches, num_batches, len(sents1)
