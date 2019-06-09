b08b5f26e0fb358697c33d9035b813604f443a89
Kasper Wendel <kasperwendel@gmail.com>
2019-06-09 17:22:02 +0200

    removed unused code

--- models/cnn.py
+++ models/cnn.py
@@ -1,45 +1,18 @@
 from os import path
 
 from tensorflow.python.keras.applications import VGG16
-from tensorflow.python.keras.layers import Reshape, Dense
+from tensorflow.python.keras.layers import Reshape
 from tensorflow.python.keras.models import Model
-
-from utils.hparams import Hparams
 
 PRETRAINED_VGG16 = 'pretrained/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
 
 
-class CNN:
-    def __init__(self, hp):
-        self.hp = hp
-
-        # TODO: why is the Dense output size (*,1000)?
-        # I think this should be d_model, the parameter that sets the size of the hidden state
-
-        # TODO/IDEA: Can we simplify this model?
-        # For each picture, we can make predictions with VGG and save them -> (#pictures, 4096) (or whatever size VGG outputs in the final layer)
-        # Now use these predictions as input data. Now we only need to do Relu + FF for an embedding
-
-        # Construct the CNN model by deleting the last layer and add Relu + FF
-        model = VGG16()
-        new_input = model.input
-        semantic_output = model.layers[-1].output
-        hidden_layer = model.layers[-6].output
-        hidden_layer = Reshape((196, 512), input_shape=(14, 14, 512))(hidden_layer)
-        spatial_output = Dense(1000, activation='relu', input_shape=(196, 512))(hidden_layer)
-        # new_output = Reshape((196, 512), input_shape = (14, 14, 512))(hidden_layer)
-
-        self.vgg = Model(inputs=new_input, outputs=[spatial_output, semantic_output])
-        self.vgg.summary()
-
-    def encode(self, xs):
-        x, seqlens, sents1 = xs
-        y_hat = self.vgg.predict(x)
-
-        return y_hat, sents1
-
-
 class DataPredictor:
+    """
+    DataPredictor is used as a preprocess step to speed up network training.
+    It uses a VGG16 with pretrained ImageNet weights to gather the intermediate predictions
+    """
+
     def __init__(self):
         # Construct the pretrained VGG16
         dir = path.dirname(__file__)
